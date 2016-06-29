2ce65355867ddf474d4289e6196d989bd3dcfb1f
Cyrus Behroozi <cyrus.behr@gmail.com>
2016-06-28 22:38:45 -0700

    updated launch screen

--- Trump Dodge/GameScene.m
+++ Trump Dodge/GameScene.m
@@ -1199,7 +1199,7 @@ static inline CGVector radiansToVector(CGFloat radians){
     
     [self showAd];
     numDeaths = 0;
-    threshold = [self getRanNum:3]+1;
+    threshold = [self getRanNum:2]+1;
     
   }
   //[restartBut setBackgroundImage:[UIImage imageNamed:@"turqois"] forState:UIControlStateNormal];
